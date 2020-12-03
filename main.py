c88a415e4d13584ef814a371aacb6f18f9f95f7d
nirgilis <p.j.vonk@uu.nl>
2020-12-03 11:43:54 +0100

    edited by autopep8

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ Created on Fri May  3 10:53:04 2019
 
 @author: pjvonk
 """
-#this program requires the following things:
-#biopython installed
-#bowtie and bowtie_build in PATH (NOTE: this is bowtie, not bowtie2)
+# this program requires the following things:
+# biopython installed
+# bowtie and bowtie_build in PATH (NOTE: this is bowtie, not bowtie2)
 
 import os
 from Bio import SeqIO
@@ -19,26 +19,29 @@ import argparse
 import glob
 import shutil
 
-#Define variables
+# Define variables
 input_file = "./data/raw/input_sequences.fasta"
 genome_assembly = "./data/raw/Schco3.assembly.fasta"
 database = "Schco3"
 
 
-# Confirm that bowtie is in the PATH. If it is, then extract the directory of the executable. This is needed by CCTOP. NOTE: this is bowtie, not bowtie2!
+# Confirm that bowtie is in the PATH. If it is, then extract the directory
+# of the executable. This is needed by CCTOP. NOTE: this is bowtie, not
+# bowtie2!
 bowtie_path = shutil.which("bowtie")
 if not bowtie_path:
     raise Exception("the executable 'bowtie' is not in your PATH variable")
 bowtie_location = os.path.dirname(bowtie_path)
 
-#run bowtie
+# run bowtie
 os.system("bowtie-build %s ./data/temp/%s" % (genome_assembly, database))
 
-#run cctop
-cctop_command = "python ./bin/cctop_standalone/CCTop.py --input %s --index ./data/temp/%s --bowtie %s --pam NGG --sgRNA5 NN --totalMM 4 --output ./data/temp/" % (input_file, database, bowtie_location)
+# run cctop
+cctop_command = "python ./bin/cctop_standalone/CCTop.py --input %s --index ./data/temp/%s --bowtie %s --pam NGG --sgRNA5 NN --totalMM 4 --output ./data/temp/" % (
+    input_file, database, bowtie_location)
 os.system(cctop_command)
 
-#parse results and output the required files
-##todo
+# parse results and output the required files
+# todo
 test = "lalala"
 test2 = "lololo"
