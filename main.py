ac3d0866f9462a97982a8d91f54957f8db23276e
nirgilis <p.j.vonk@uu.nl>
2020-12-03 12:47:14 +0100

    added two functions and tidied up the CCTop command

--- main.py
+++ main.py
@@ -29,11 +29,46 @@ bowtie_location = os.path.dirname(bowtie_path)
 os.system("bowtie-build %s ./data/temp/%s" % (genome_assembly, database))
 
 # run cctop
-cctop_command = """
-    python ./bin/cctop_standalone/CCTop.py --input %s --index ./data/temp/%s 
-    --bowtie %s --pam NGG --sgRNA5 NN --totalMM 4 --output ./data/temp/
-    """ % (input_file, database, bowtie_location)
+# TODO: change to format {}
+cctop_command = "python ./bin/cctop_standalone/CCTop.py "\
+                "--input {input_file} "\
+                "--index ./data/temp/{database} "\
+                "--bowtie {bowtie_location} "\
+                "--pam NGG "\
+                "--sgRNA5 NN "\
+                "--totalMM 4 "\
+                "--output ./data/temp/".format(
+                input_file=input_file,
+                database=database,
+                bowtie_location=bowtie_location)
+                
 os.system(cctop_command)
 
 # parse results and output the required files
 # TODO: Write code to parse output of CCTop
+def reverse_complement(protospacer):
+    reverse_protospacer = ""
+    
+    for nt in protospacer.strip()[::-1]:
+        if nt == "A":
+            reverse_protospacer.append("T")
+        elif nt == "T":
+            reverse_protospacer.append("A")
+        elif nt == "C":
+            reverse_protospacer.append("G")
+        elif nt == "G":
+            reverse_protospacer.append("C")
+        else:
+            raise Exception("invalid character in protospacer. Please check your input sequences")
+    return(reverse_protospacer)
+
+def design_primers_sgRNA(protospacer):
+    fw_addition = "TAATACGACTCACTATAG"
+    rv_addition = "TTCTAGCTCTAAAAC"
+    if protospacer[0] == "G":
+        fw_primer = fw_addition + protospacer
+    else:
+        fw_primer = fw_addition + "G" + "protospacer"
+    rc_protospacer = reverse_complement(protospacer)
+    rv_primer = rv_addition + rc_protospacer
+    return(fw_primer, rv_primer)
