4cc2f96170474ee90ab41eeab6eff2906af68326
nirgilis <p.j.vonk@uu.nl>
2020-12-04 08:53:02 +0100

    added file parser

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ Created on Fri May  3 10:53:04 2019
 # bowtie and bowtie_build in PATH (NOTE: this is bowtie, not bowtie2)
 import os
 import shutil
+from Bio import SeqIO
 
 # Define variables
 input_file = "./data/raw/input_sequences.fasta"
@@ -37,14 +38,15 @@ cctop_command = ("python ./bin/cctop_standalone/CCTop.py "
                 "--sgRNA5 NN "
                 "--totalMM 4 "
                 "--output ./data/temp/".format(
-                input_file=input_file,
-                database=database,
-                bowtie_location=bowtie_location))
+                input_file = input_file,
+                database = database,
+                bowtie_location = bowtie_location))
                 
 os.system(cctop_command)
 
 # parse results and output the required files
 # TODO: Write code to parse output of CCTop
+
 def reverse_complement(protospacer):
     reverse_protospacer = ""
     
@@ -58,7 +60,8 @@ def reverse_complement(protospacer):
         elif nt == "G":
             reverse_protospacer.append("C")
         else:
-            raise Exception("invalid character in protospacer. Please check your input sequences")
+            raise Exception("invalid character in protospacer. "
+                            "Please check your input sequences")
     return(reverse_protospacer)
 
 def design_primers_sgRNA(protospacer):
@@ -71,3 +74,55 @@ def design_primers_sgRNA(protospacer):
     rc_protospacer = reverse_complement(protospacer)
     rv_primer = rv_addition + rc_protospacer
     return(fw_primer, rv_primer)
+
+#identify sequences in fasta
+genes_analyzed = []
+for sequence in SeqIO.parse(open(input_file), "fasta"):
+    genes_analyzed.append(sequence.id)
+
+# Generate file to store data with header
+output_file = open("./results/output/results.txt", "w")
+output_file.write("ID\t"
+                  "proteinId\t"
+                  "Score\t"
+                  "off_targets\t"
+                  "gRNA\t"
+                  "fw_primer\t"
+                  "rv_primer\n")
+
+# first loop over the different genes
+for gene in genes_analyzed:
+    results_file = open("./data/temp/{}.xls".format(gene))
+    result_counter = 1
+    # Next, we loop over te results inside the file
+    for line in results_file.readlines()[9:]:
+        if line.split("\t")[0] == "T" + str(result_counter):
+            off_target_counter = -1 # since the first hit is the right hit we start with -1
+            protospacer = line.split("\t")[1][0:20]
+            raw_score = line.split("\t")[4]
+            cleaned_score = raw_score.split(" ")[0]
+            fw_primer, rv_primer = design_primers_sgRNA(protospacer)
+            result_counter += 1
+        # Identify the off_targets found for this protospacer
+        if line.split("\t")[0].split("_")[0] == "scaffold":
+            off_target_counter += 1
+        # Identify the start of a new gene
+        if line == "\n":
+            output_file.write("{ID}\t"
+                              "{gene}\t"
+                              "{score}\t"
+                              "{off_target_count}\t"
+                              "{protospacer}\t"
+                              "{fw_primer}\t"
+                              "{rv_primer}\n".format(
+                                ID = "T" + str(result_counter - 1),
+                                gene = gene,
+                                score = raw_score,
+                                off_target_count = str(off_target_counter),
+                                protospacer = protospacer,
+                                fw_primer = fw_primer,
+                                rv_primer = rv_primer))
+        
+
+
+
